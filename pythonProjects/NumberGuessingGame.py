7df91701cba9327f2615cbb72926a3f893f1a338
Suhas Julapalli <suhasjulapalli@gmail.com>
2021-09-22 15:02:08 -0400

    test

---
+++ pythonProjects/NumberGuessingGame.py
@@ -0,0 +1,6 @@
+import random
+
+print("Please make all inputs as integers. Non-integer inputs will be rejected.\n")
+min = int(input("Enter minimum range: "))
+max = int(input("Enter maximum range: "))
+random.randint(min, max)
